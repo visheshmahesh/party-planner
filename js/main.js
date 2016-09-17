97b713e26276c35f7cff3fe0ef52fcb4a5378988
Ayush <Ayush>
2016-09-17 11:59:04 +0530

    added main page and updated stylings

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function parallax () {
   header.style.top = y * 0.5 + 'px';
 }
 
-// HIDING HINT TEXT ON SCROLL //
+// SCRIPT FOR HIDING HINT TEXT ON SCROLL //
 
 window.addEventListener('scroll',hidehint);
 
@@ -20,4 +20,29 @@ function hidehint () {
   text = document.getElementById('hinttext');
   text.style.opacity="0";
   text.style.transition="1s";
+}
+
+// SCRIPT TO CHANGE VENUE DIV CONTENT //
+
+function Change() {
+  var x;
+  x = document.getElementById("venue").selectedIndex;
+  document.getElementById("venuepic").style.width = "40vmin";
+  document.getElementById("venuepic").style.height = "40vmin";
+  document.getElementById("venuepic").style.transition = "0.5s";
+  if (x==0) {
+    document.getElementById("venuedesc").innerHTML = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin varius massa. Curabitur mattis semper lorem id tempus. Phasellus nec tellus vel nibh faucibus luctus. Vestibulum sit amet sapien turpis. Proin placerat pretium lectus ac molestie. Phasellus mollis mi sed enim dignissim blandit. Nunc hendrerit mattis lectus quis blandit. In a tortor vel tortor volutpat laoreet. Nam tristique arcu a massa dignissim, in eleifend nibh ultricies.";
+    document.getElementById("venuepic").src = "../assets/images/venue/delhi/grandsaph.jpg";
+    console.log("yes");
+
+  }
+  else if (x==1) {
+    document.getElementById("venuedesc").innerHTML = "WORKING2";
+  }
+  else if (x==2) {
+    document.getElementById("venuedesc").innerHTML = "WORKING3";
+  }
+  else if (x==3) {
+    document.getElementById("venuedesc").innerHTML = "WORKING4";
+  }
 }
